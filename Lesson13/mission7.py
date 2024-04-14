53597bf357d55e95d438aa8827e234937c7493eb
skgnt <shinn14h.m0304@gmail.com>
2024-04-14 12:54:53 +0900

    update

--- Lesson13/mission7.py
+++ Lesson13/mission7.py
@@ -4,7 +4,7 @@ import random
 
 #エラー原因:関数内でmaxを変更しようとしているが、その変数が関数内で定義されていない。
 #エラー内容:UnboundLocalError: cannot access local variable 'max' where it is not associated with a value
-#解決法:関数内でmaxを定義する。
+#解決法:関数内でmaxを定義する。(グローバル宣言でも可)
 def max_value(numbers):
     max=0
     for n in numbers:
@@ -13,6 +13,7 @@ def max_value(numbers):
     return max
 
 
+#max=0
 
 
 rand_list=[random.randint(1,100) for i in range(10)]
